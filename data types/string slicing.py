07d72dc91ac58e3e4996315a8cfa1c76f375d9cb
akhilayarabolu <akhilayarabolu17@gmail.com>
2026-08-02 22:00:19 +0530

    Delete data types directory

--- data types/string slicing.py
+++
@@ -1,9 +0,0 @@
-#dharani satya
-#A2.2 - string slicing
-first = "Dharani"
-last = "Satya"
-full_name = first + " " + last
-print("First Name:", full_name[:7])
-print("Last Name:", full_name[8:])
-print("First 5 Characters:", full_name[:5])
-# First Name: Dharani
