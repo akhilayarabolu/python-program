4ce7f5a52555f7ad5fbd8e35177bdec18b373498
akhilayarabolu <akhilayarabolu17@gmail.com>
2026-08-02 22:05:30 +0530

    Add files via upload

---
+++ data types/strings.py
@@ -0,0 +1,19 @@
+#akhila
+#A 2.1 - strings
+first = "akhila"
+last = "yarabolu"
+full_name = first + " " + last
+print("Full Name:", full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print("Length:", len(full_name))
+print("First Character:", full_name[0])
+print("Last Character:", full_name[-1])
+# Full Name: akhila yarabolu
+# AKHILA YARABOLU
+# akhila yarabolu
+# AKHILA YARABOLU
+# Length: 14
+# First Character: A
+# Last Character: U
