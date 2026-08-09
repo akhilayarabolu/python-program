65a470108a51bd0a8f694f3e92c74aee96c68d94
akhilayarabolu <akhilayarabolu17@gmail.com>
2026-08-09 23:27:59 +0530

    Update username.py

--- case study/username.py
+++ case study/username.py
@@ -2,3 +2,9 @@ first_name=input("enter your name:")
 roll_number=input("enter your roll number:")
 user_name=first_name.lower()+roll_number[-2:]
 print("generated user_name:",user_name)
+output:
+enter your name:akhila
+enter your roll number:25341A05N6
+generated user_name: akhilaN6
+
+
