ceae3b39e6ef9916827786cfe53d3de563808840
akhilayarabolu <akhilayarabolu17@gmail.com>
2026-08-09 23:24:18 +0530

    Update schlorship.py

--- case study/schlorship.py
+++ case study/schlorship.py
@@ -2,3 +2,9 @@ percentage = int(input("enter percentage: "))
 income =int(input("enter income: "))
 eligible = percentage > 85 or (percentage > 75 and income < 200000)
 print("Eligible for scholarship:", eligible)
+output:
+enter percentage: 94
+enter income: 20000
+Eligible for scholarship: True
+
+
