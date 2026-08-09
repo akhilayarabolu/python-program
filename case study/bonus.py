dd2aa50763bc704fc504e74ca6a3930ae10d6c50
akhilayarabolu <akhilayarabolu17@gmail.com>
2026-08-09 23:17:56 +0530

    Update bonus.py

--- case study/bonus.py
+++ case study/bonus.py
@@ -2,3 +2,9 @@ salary=80000
 for year in range(1,4):
     salary+=salary*0.10
     print("salary after year",year,":",salary)
+    output:
+ salary after year 1 : 88000.0
+salary after year 2 : 96800.0
+salary after year 3 : 106480.0
+
+    
