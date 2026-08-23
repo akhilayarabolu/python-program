867efca14d01584e89824da7f77851eecf7ec6c2
akhilayarabolu <akhilayarabolu17@gmail.com>
2026-08-23 22:07:24 +0530

    Add files via upload

---
+++ loops/pyramid.py
@@ -0,0 +1,16 @@
+# y akhila
+#pyramid program
+n = int(input("Enter number of rows: "))
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end=" ")
+    for j in range(i):
+        print("*", end=" ")
+    print()
+    output:
+    #Enter number of rows: 5
+        #* 
+      #* * 
+    #* * * 
+  #* * * * 
+#* * * * * 
---
+++ loops/right angled.py
@@ -0,0 +1,14 @@
+# y akhila
+# right-angled triangle program
+n = int(input("Enter number of rows: "))
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+    output:
+    #Enter number of rows: 5
+#* 
+#* * 
+#* * * 
+#* * * * 
+#* * * * * 
